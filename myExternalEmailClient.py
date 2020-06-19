c0aa293632c0238c15567f6cf7199a7fe55d76ee
Mahdokht Afravi <mmafravi@gmail.com>
2020-06-19 15:53:57 -0600

    created client python scripts to begin assignment

---
+++ myExternalEmailClient.py
@@ -0,0 +1,6 @@
+""" Using sockets, connect to a popular webmail server,
+      like AOL mail server.
+    Then, perform the same task as Part I
+      and implement the SMTP conversations in the python program
+      which will contact with the SMTP server to send your email
+      to "your_email@gmail.com"."""
---
+++ myUtepEmailClient.py
@@ -0,0 +1,4 @@
+""" Using sockets, connect to campus SMTP mail server.
+    Then implement the SMTP conversations in the python program,
+      which will contact with the server to send your email
+      to "your_email@utep.edu". """
