54abe7180fe7dbeab45f0e9c783e9b3e024fcab4
Mahdokht Afravi <mmafravi@gmail.com>
2020-06-22 14:56:21 -0600

    part1 added w/error handling; todo (1) log in to UTEP server and (2) send email(s); part2 untouched though uses same code pas part1 (mostly)

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ In this task, you will be implementing a python-based simple mail client, which 
 _Note_: You are not allowed to use the `smtplib` python library.
 
 ## Part I
-Using sockets, connect to campus SMTP mail server. Then implement the smtp conversations in the python program `myUtepEmailClient.` which will contact with the server to send your email to "your_email@utep.edu". 
+Using sockets, connect to campus SMTP mail server. Then implement the smtp conversations in the python program `myUtepEmailClient` which will contact with the server to send your email to "your_email@utep.edu". 
+- From [Microsoft's website](https://support.microsoft.com/en-us/office/pop-and-imap-email-settings-for-outlook-8361e398-8af4-4e97-b147-6c6c4ac95353?ui=en-us&rs=en-us&ad=us), the following settings are used for UTEP's Office365 SMTP settings:
+    - Server: smtp.office365.com
+    - Port: 587
+    - Encryption: STARTTLS
+- To create the mail server, I followed [this tutorial](realpython.com/python-sockets/)
+- For the syntax on socket communications when it comes to logging in, I used [this resource](https://stackoverflow.com/questions/33397024/mail-client-in-python-using-sockets-onlyno-smtplib) to format my messages to send to the connection.
+    - does UTEP have issues because of SSO?
+    - or is it a setting I'd need to contact tech support for? (see [this](https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/authenticated-client-smtp-submission) for more infoormation)
 
 ## Part II
 Using sockets, connect to a popular webmail server, like AOL mail server. Then perform the same task as Part-I and implement the smtp conversations in the python program `myExternalEmailClient.py` which will contact with the SMTP server to send your email to "your_email@gmail.com".
--- myUtepEmailClient.py
+++ myUtepEmailClient.py
@@ -2,3 +2,79 @@
     Then implement the SMTP conversations in the python program,
       which will contact with the server to send your email
       to "your_email@utep.edu". """
+import base64
+import socket
+
+s_addr = 'smtp.aol.com' # 'smtp.office365.com'
+s_port = 465 #587
+
+class UTEPEmailServer:
+    def __init__(self, addr, prt):
+        self.__server = None
+        # utep email service
+        self.__addr = addr
+        self.__prt = prt
+
+    """ Opens an internet stream to the addr:prt specified. """
+    def __start_connection(self):
+        self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server.connect((self.__addr, self.__prt))
+        msg = self.__decode_receive()
+        self.__check_closed(msg)
+        if '220' not in msg:    # could not open connection
+            error_report_and_quit('Could not open connection to\t' + str(self.__addr))
+
+    """ Establish the connection by sending/receiving 'hello'/authentication message. """
+    def establish_connection(self):
+        self.__start_connection()
+        self.__server.send('EHLO\r\n'.encode())
+        msg = self.__decode_receive()
+        self.__check_closed(msg)
+        if '250' not in msg:        # 'hello' not received
+            error_report_and_quit('Could not establish connection to\t' + str(self.__addr))
+        print('Established connection to:\t' + self.__addr)
+
+    """ Logs into the server using provided credentials """
+    def log_in(self, user, pswd):
+        b64 = base64.b64encode(("\x00"+user+"\x00"+pswd).encode())
+        self.__server.send("AUTH PLAIN ".encode() + b64 + "\r\n".encode())
+        msg = self.__decode_receive()
+        self.__check_closed(msg)
+        return msg
+
+    """ Close the connection. """
+    def close(self):
+        self.__server.shutdown()
+        self.__server.close()
+        print('Closed connection.')
+
+    """ Decodes the message received from base64. """
+    def __decode_receive(self, msg=1024):
+        return self.__server.recv(msg).decode()   # decode from base64
+
+    """ Has the connection closed abruptly? """
+    def __check_closed(self, msg):
+        if not msg:             # connection closed
+            error_report_and_quit()
+
+
+""" Report error message to console and quit program unsuccessfully. """
+def error_report_and_quit(msg='Connection was closed.'):
+    print('ERROR: ' + msg)
+    exit(1)
+
+if __name__ == '__main__':
+    # open the connection
+    server = UTEPEmailServer(s_addr, s_port)
+    server.establish_connection()
+
+    # log in to user-provided credentials
+    msg = '504'
+    while '504' in msg:
+        user = input('Enter your username for ' + s_addr + ':\t')
+        pswd = input('Enter your password for ' + user + ':\t')
+        msg = server.log_in(user, pswd)
+        print(msg)
+    print('Successfully logged in to:\t' + user + ' at ' + s_addr)
+
+    server.close()
